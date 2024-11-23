68c9150c07f449970c3c67e5e4469cb36b023a62
mohdumer7 <mohammed@inspiritvr.com>
2024-11-23 23:32:39 +0400

    tg check 2

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -38,6 +38,10 @@ const HomePage = () => {
         // Send Telegram user info to the backend
         const { data } = await axios.post('/api/onboard', telegramUser)
 
+        toast.success(`Welcome ${data}`, {
+          description: data.username
+        } as any)
+
         setUser(data) // Store the user info in state
       } catch (error) {
         console.error('Error during user onboarding:', error)
