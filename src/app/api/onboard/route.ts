ce735f788547b9d0ea1fa33a053941f72c02268d
mohdumer7 <mohammed@inspiritvr.com>
2024-11-23 22:01:39 +0400

    added bot config

--- src/app/api/onboard/route.ts
+++ src/app/api/onboard/route.ts
@@ -1,19 +1,15 @@
-// pages/api/onboard.js
-import connectToDatabase from '@/utils/db'
-import User from '@/models/User'
+import { NextApiRequest, NextApiResponse } from 'next';
+import connectToDatabase from '@/utils/db';
+import User from '@/models/User';
 
-export default async function handler(req, res) {
-  if (req.method !== 'POST') {
-    return res.status(405).json({ message: 'Only POST requests are allowed' })
-  }
-
-  const { telegramId, firstName, lastName } = req.body
+export default async function POST(req: NextApiRequest, res: NextApiResponse) {
+  const { telegramId, firstName, lastName } = req.body;
 
   try {
-    await connectToDatabase()
+    await connectToDatabase();
 
     // Check if the user already exists
-    let user = await User.findOne({ telegramId })
+    let user = await User.findOne({ telegramId });
 
     if (!user) {
       // Create a new user
@@ -23,12 +19,12 @@ export default async function handler(req, res) {
         lastName,
         walletBalance: 0,
         rewardsBalance: 0,
-      })
+      });
     }
 
-    res.status(200).json(user)
+    res.status(200).json(user);
   } catch (error) {
-    console.error('Error onboarding user:', error)
-    res.status(500).json({ message: 'Internal server error' })
+    console.error('Error onboarding user:', error);
+    res.status(500).json({ message: 'Internal server error' });
   }
 }
